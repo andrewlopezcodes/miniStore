c8762521205c6bbdd4b22a630b2cd3eb7376d4c4
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-11 19:40:06 -0400

    index.html - Installed script with source of miniStore.js - miniStore.js - configure basic express router on port 3000.

--- miniStore.js
+++ miniStore.js
@@ -1,1 +1,21 @@
-console.log('hello there');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello there')
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
